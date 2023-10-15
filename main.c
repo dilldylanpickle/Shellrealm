125b9c085f6a033e0afdaf90461244070bac2da6
dilldylanpickle <dgonzales4055@gmail.com>
2023-10-14 21:23:08 -0500

    Added process spawner, global debug flag, and a command executer

--- main.c
+++ main.c
@@ -1,6 +1,18 @@
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+
+#define debug_on 1
+
+#if debug_on
+#define debugf(...) printf(__VA_ARGS__)
+#else
+#define debugf(...)
+#endif
+
 
 char *shr_readline(void)
 {
@@ -17,7 +29,8 @@ char **shr_readargs(char *line)
     int index = 0;
     char **arguments = malloc(64 * sizeof(char*));
     char *command;
-    printf("[DEBUG] Line to tokenize: %s", line);
+    
+    debugf("\t[DEBUG] Standard Input: %s", line);
 
     command = strtok(line, " \t\r\n\a");
     while (command != NULL) {
@@ -28,6 +41,33 @@ char **shr_readargs(char *line)
 
     arguments[index] = NULL;
     return arguments;
+}
+
+int shr_execve(char *arguments[]) 
+{
+    int status = 1;
+    pid_t pid;
+
+    if (arguments[0] == NULL) {
+        debugf("\t[DEBUG] No arguments provided\n");
+        return 1;
+    }
+
+    pid = fork();
+
+    if (pid == 0) {
+        debugf("\t[DEBUG] Executing command: %s\n", arguments[0]);
+        execvp(arguments[0], arguments);
+        perror("shr");
+        exit(EXIT_FAILURE);
+    } else if (pid < 0) {
+        perror("shr");
+        return 1;
+    } else {
+        wait(NULL);
+    }
+
+    return status;
 }
 
 void shellrealm(void)
@@ -43,10 +83,13 @@ void shellrealm(void)
 
         int index = 0;
         while (arguments[index] != NULL) {
-            printf("[DEBUG] Argument %d: %s\n", index, arguments[index]);
+            debugf("\t[DEBUG] Argument %d: %s\n", index, arguments[index]);
             index++;
         }
 
+        status = shr_execve(arguments);
+
+        free(line);
         free(arguments);
 
     } while (status);
