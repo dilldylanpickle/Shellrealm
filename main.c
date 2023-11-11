11e69c21cc44bcd39a81972d1de7c1ff0e0d2fd7
dilldylanpickle <dgonzales4055@gmail.com>
2023-11-10 18:42:30 -0600

    Incorporated a few basic commands like exit and help

--- main.c
+++ main.c
@@ -13,39 +13,91 @@
 #define debugf(...)
 #endif
 
-void shr_title()
-{
+int shr_exit(char **arguments);
+int shr_version(char **arguments);
+int shr_help(char **arguments);
+
+typedef struct {
+    char *name;
+    int (*func)(char **);
+} shrcmds;
+
+shrcmds commands[] = {
+    {"exit", shr_exit},
+    {"version", shr_version},
+    {"help", shr_help},
+};
+
+int cmds = sizeof(commands) / sizeof(shrcmds);
+
+void shr_title() {
+    printf("\n");
     printf("███████╗██╗  ██╗███████╗██╗     ██╗     ██████╗ ███████╗ █████╗ ██╗     ███╗   ███╗\n");
     printf("██╔════╝██║  ██║██╔════╝██║     ██║     ██╔══██╗██╔════╝██╔══██╗██║     ████╗ ████║\n");
     printf("███████╗███████║█████╗  ██║     ██║     ██████╔╝█████╗  ███████║██║     ██╔████╔██║\n");
     printf("╚════██║██╔══██║██╔══╝  ██║     ██║     ██╔══██╗██╔══╝  ██╔══██║██║     ██║╚██╔╝██║\n");
     printf("███████║██║  ██║███████╗███████╗███████╗██║  ██║███████╗██║  ██║███████╗██║ ╚═╝ ██║\n");
     printf("╚══════╝╚═╝  ╚═╝╚══════╝╚══════╝╚══════╝╚═╝  ╚═╝╚══════╝╚═╝  ╚═╝╚══════╝╚═╝     ╚═╝\n");
-
     printf("\nYou have entered the shellrealm!\n\n");
-
 }
 
-char *shr_readline(void)
-{
-  char *line = NULL;
-  ssize_t bufsize = 0;
-
-  getline(&line, &bufsize, stdin);
-
-  return line;
+int shr_help(char **arguments) {
+    printf("Shellrealm commands:\n");
+    for (int i = 0; i < sizeof(commands) / sizeof(shrcmds); i++) {
+        printf(" - %s\n", commands[i].name);
+    }
+    return 1;
 }
 
-char **shr_readargs(char *line)
-{
-    int index = 0;
-    char **arguments = malloc(64 * sizeof(char*));
+int shr_exit(char **arguments) {
+    printf("Exiting Shellrealm...\n");
+    return 0;
+}
+
+int shr_version(char **arguments) {
+    printf("Shellrealm version 1.0\n");
+    return 1;
+}
+
+char *shr_readline(void) {
+    char *line = NULL;
+    size_t bufsize = 0;
+
+    if (getline(&line, &bufsize, stdin) == -1) {
+        if (!feof(stdin)) {
+            perror("getline failed");
+        }
+        free(line);
+        return NULL;
+    }
+
+    return line;
+}
+
+char **shr_readargs(char *line) {
+    int bufsize = 64, index = 0;
+    char **arguments = malloc(bufsize * sizeof(char*));
     char *command;
+
+    if (!arguments) {
+        fprintf(stderr, "allocation error\n");
+        exit(EXIT_FAILURE);
+    }
 
     command = strtok(line, " \t\r\n\a");
     while (command != NULL) {
         arguments[index] = command;
         index++;
+
+        if (index >= bufsize) {
+            bufsize += 64;
+            arguments = realloc(arguments, bufsize * sizeof(char*));
+            if (!arguments) {
+                fprintf(stderr, "allocation error\n");
+                exit(EXIT_FAILURE);
+            }
+        }
+
         command = strtok(NULL, " \t\r\n\a");
     }
 
@@ -53,34 +105,24 @@ char **shr_readargs(char *line)
     return arguments;
 }
 
-int shr_execve(char *arguments[]) 
-{
-    int status = 1;
-    pid_t pid;
+int shr_execve(char *arguments[]) {
 
     if (arguments[0] == NULL) {
         debugf("\t[DEBUG] No arguments provided\n");
         return 1;
     }
 
-    pid = fork();
-
-    if (pid == 0) {
-        execvp(arguments[0], arguments);
-        perror("shr");
-        exit(EXIT_FAILURE);
-    } else if (pid < 0) {
-        perror("shr");
-        return 1;
-    } else {
-        wait(NULL);
+    for (int i = 0; i < cmds; i++) {
+        if (strcmp(arguments[0], commands[i].name) == 0) {
+            return commands[i].func(arguments);
+        }
     }
 
-    return status;
+    printf("%s: command not found\n", arguments[0]);
+    return 1;
 }
 
-void shellrealm(void)
-{
+void shellrealm(void) {
     char *line;
     char **arguments;
     int status;
@@ -90,18 +132,19 @@ void shellrealm(void)
     do {
         printf("(shellrealm) ");
         line = shr_readline();
+        if (!line) {
+            break;
+        }
+
         arguments = shr_readargs(line);
         status = shr_execve(arguments);
 
         free(line);
         free(arguments);
-
     } while (status);
 }
 
-int main(int argc, char *argv[])
-{
+int main(int argc, char *argv[]) {
     shellrealm();
-
     return 0;
 }
